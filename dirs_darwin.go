fb7406a40e6f2165d2912514051cc774362807a1
Rishvic Pushpakaran <rishvic@gmail.com>
2022-07-06 12:15:29 +0530

    Added paths for Darwin

---
+++ dirs_darwin.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2022 Rishvic Pushpakaran
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package dirs
+
+import (
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+)
+
+func HomeDir() (string, error) { return homedir.Dir() }
+
+func CacheDir() (string, error)      { return joinHome("Library", "Caches") }
+func ConfigDir() (string, error)     { return joinHome("Library", "Application Support") }
+func DataDir() (string, error)       { return joinHome("Library", "Application Support") }
+func DataLocalDir() (string, error)  { return DataDir() }
+func PreferenceDir() (string, error) { return joinHome("Library", "Preferences") }
+func ExecutableDir() (string, error) { return "", ErrNotFound }
+func RuntimeDir() (string, error)    { return "", ErrNotFound }
+func StateDir() (string, error)      { return "", ErrNotFound }
+
+func AudioDir() (string, error)    { return joinHome("Music") }
+func DesktopDir() (string, error)  { return joinHome("Desktop") }
+func DocumentDir() (string, error) { return joinHome("Documents") }
+func DownloadDir() (string, error) { return joinHome("Downloads") }
+func FontDir() (string, error)     { return joinHome("Library", "Fonts") }
+func PictureDir() (string, error)  { return joinHome("Pictures") }
+func PublicDir() (string, error)   { return joinHome("Public") }
+func TemplateDir() (string, error) { return "", ErrNotFound }
+func VideoDir() (string, error)    { return joinHome("Movies") }
+
+func joinHome(path ...string) (string, error) {
+	homeDir, err := HomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	elem := []string{homeDir}
+	elem = append(elem, path...)
+	return filepath.Join(elem...), nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Colocasian/dirs-go
 
 go 1.18
 
-require golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e
+)
